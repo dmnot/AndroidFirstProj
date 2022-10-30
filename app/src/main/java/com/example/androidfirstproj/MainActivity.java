1975ba8926bd07e134b2756b9e7bbe8a617cfd3e
dmnot <tomatsuper1701@gmail.com>
2022-10-31 04:00:29 +0500

    Added Java Screen Configuration example

--- app/src/main/java/com/example/androidfirstproj/MainActivity.java
+++ app/src/main/java/com/example/androidfirstproj/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.androidfirstproj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+
+        //Text for app
+        TextView textView = new TextView(this);
+        textView.setText("Hi Android");
+        textView.setTextSize(22);
+        //---------------------------------------
+        setContentView(textView);
+
     }
+
+
 }
