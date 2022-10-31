f15255bc347d1c902b14db556d397de5b2a0c8e0
dmnot <tomatsuper1701@gmail.com>
2022-10-31 09:49:24 +0500

    Added Java Screen Configuration example with ConstraintLayout

--- app/src/main/java/com/example/androidfirstproj/MainActivity.java
+++ app/src/main/java/com/example/androidfirstproj/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.androidfirstproj;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.widget.TextView;
@@ -11,14 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-
-
         //Text for app
         TextView textView = new TextView(this);
         textView.setText("Hi Android");
         textView.setTextSize(22);
+        //-------------------------------
+        //ConstraintLayout
+        ConstraintLayout constraintLayout = new ConstraintLayout(this);
+        //--------------------------------
+        //Params for ConstraintLayout
+        //Размер контейнера в зависимости от положенного внутрь контента
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        //------------------------------------------------------------------
+        // Позиционирование текста по центру
+        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
+        //----------------------------------------------------------------
+        textView.setLayoutParams(layoutParams); //Применение параметров к тексту
+        constraintLayout.addView(textView); //Закидывание внутрь ConstraintLayout'a TextView
         //---------------------------------------
-        setContentView(textView);
+        setContentView(constraintLayout);
 
     }
 
